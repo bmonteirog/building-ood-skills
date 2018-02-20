15b4faac1610ef824f547fe964500aea2640c5e4
Bruno <bmonteirog@gmail.com>
2018-02-19 22:01:44 -0300

    BinBuilder Class and tests

--- roulette/src/BinBuilder.php
+++ roulette/src/BinBuilder.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 declare(strict_types=1);
 
 namespace Roulette;
@@ -11,12 +11,12 @@ use Roulette\Outcome;
  */
 class BinBuilder
 {
-  
+
   /**
    * @var Wheel
    */
    protected $wheel;
-  
+
   /**
    * Wheel that must be populated with Outcomes
    *
@@ -27,7 +27,7 @@ class BinBuilder
   public function buildBins(Wheel $wheel)
   {
     $this->wheel = $wheel;
-    
+
     $this->generateStraightBets();
     $this->generateSplitBets();
     $this->generateStreetBets();
@@ -36,37 +36,36 @@ class BinBuilder
     $this->generateDozenBets();
     $this->generateColumnBets();
     $this->generateEvenMoneyBets();
-    $this->generateZeroBets();
-    $this->generateDoubleZeroBets();
-    
+    $this->generateFiveBet();
+
     return $this->wheel;
   }
-  
+
   /**
    * Create all straight bets possible for the wheel
    **/
-  private function generateStraightBets() 
+  private function generateStraightBets()
   {
-    for ($i = 0; $i <= 37; $i++) { 
+    for ($i = 0; $i <= 37; $i++) {
       $outcomeName = ($i < 37) ? "{$i}" : "00";
       $this->wheel->addOutcome($i, new Outcome($outcomeName, 35));
     }
   }
-  
+
   /**
    * Create all split bets possible for the wheel
    **/
-  private function generateSplitBets() 
+  private function generateSplitBets()
   {
     for ($row = 0; $row < 12; $row++) {
       $this->createLeftRightSplits($row);
     }
-    
-    for ($number = 1; $number <= 33; $number++) { 
-      $this->createUpDownSplits($number);      
+
+    for ($number = 1; $number <= 33; $number++) {
+      $this->createUpDownSplits($number);
     }
   }
-  
+
   /**
    * Create left-right splits for the columns 1 and 2
    **/
@@ -77,14 +76,14 @@ class BinBuilder
     $firstColumnSplitOutcome = new Outcome("Split {$firstColumnNumber}-{$neighbor}", 17);
     $this->wheel->addOutcome($firstColumnNumber, $firstColumnSplitOutcome);
     $this->wheel->addOutcome($neighbor, $firstColumnSplitOutcome);
-    
+
     $secondColumnNumber = (3 * $row) + 2;
     $secondNeighbor = $secondColumnNumber + 1;
     $secondColumnSplitOutcome = new Outcome("Split {$secondColumnNumber}-{$secondNeighbor}", 17);
     $this->wheel->addOutcome($secondColumnNumber, $secondColumnSplitOutcome);
     $this->wheel->addOutcome($secondNeighbor, $secondColumnSplitOutcome);
   }
-  
+
   /**
    * Create up-down splits
    **/
@@ -93,15 +92,15 @@ class BinBuilder
     $neighbor = $number + 3;
     $splitOutcome = new Outcome("Split {$number}-{$neighbor}", 17);
     $this->wheel->addOutcome($number, $splitOutcome);
-    $this->wheel->addOutcome($neighbor, $splitOutcome);    
+    $this->wheel->addOutcome($neighbor, $splitOutcome);
   }
-  
+
   /**
    * Create all street bets possible for the wheel
    **/
-  private function generateStreetBets() 
+  private function generateStreetBets()
   {
-    for ($row = 0; $row < 12; $row++) { 
+    for ($row = 0; $row < 12; $row++) {
       $firstNumber = (3 * $row) + 1;
       $secondNumber = $firstNumber + 1;
       $thirdNumber = $firstNumber + 2;
@@ -111,61 +110,159 @@ class BinBuilder
       $this->wheel->addOutcome($thirdNumber, $streetOutcome);
     }
   }
-  
+
   /**
    * Create all corner bets possible for the wheel
    **/
-  private function generateCornerBets() 
+  private function generateCornerBets()
   {
-    
+    for ($row = 0; $row < 11; $row++) {
+      $firstColNumber = (3 * $row) + 1;
+      $firstColRightNeighbor = $firstColNumber + 1;
+      $firstColDownNeighbor = $firstColNumber + 3;
+      $firstColDownRightNeighbor = $firstColNumber + 4;
+      $firstColCornerOutcome = new Outcome("Corner {$firstColNumber}-{$firstColRightNeighbor}-{$firstColDownNeighbor}-{$firstColDownRightNeighbor}", 8);
+      $this->wheel->addOutcome($firstColNumber, $firstColCornerOutcome);
+      $this->wheel->addOutcome($firstColRightNeighbor, $firstColCornerOutcome);
+      $this->wheel->addOutcome($firstColDownNeighbor, $firstColCornerOutcome);
+      $this->wheel->addOutcome($firstColDownRightNeighbor, $firstColCornerOutcome);
+
+      $secondColNumber = (3 * $row) + 2;
+      $secondColRightNeighbor = $secondColNumber + 1;
+      $secondColDownNeighbor = $secondColNumber + 3;
+      $secondColDownRightNeighbor = $secondColNumber + 4;
+      $secondColCornerOutcome = new Outcome("Corner {$secondColNumber}-{$secondColRightNeighbor}-{$secondColDownNeighbor}-{$secondColDownRightNeighbor}", 8);
+      $this->wheel->addOutcome($secondColNumber, $secondColCornerOutcome);
+      $this->wheel->addOutcome($secondColRightNeighbor, $secondColCornerOutcome);
+      $this->wheel->addOutcome($secondColDownNeighbor, $secondColCornerOutcome);
+      $this->wheel->addOutcome($secondColDownRightNeighbor, $secondColCornerOutcome);
+    }
   }
-  
+
   /**
    * Create all line bets possible for the wheel
    **/
-  private function generateLineBets() 
+  private function generateLineBets()
   {
-    
+    for ($row = 0; $row < 11; $row++) {
+      $number = (3 * $row) + 1;
+      $rightNeighbor = $number + 1;
+      $rightRightNeighbor = $number + 2;
+      $downNeighbor = $number + 3;
+      $downRightNeighbor = $number + 4;
+      $downRightRightNeighbor = $number + 5;
+      $lineOutcome = new Outcome("Line {$number}-{$rightNeighbor}-{$rightRightNeighbor}-{$downNeighbor}-{$downRightNeighbor}-{$downRightRightNeighbor}", 5);
+      $this->wheel->addOutcome($number, $lineOutcome);
+      $this->wheel->addOutcome($rightNeighbor, $lineOutcome);
+      $this->wheel->addOutcome($rightRightNeighbor, $lineOutcome);
+      $this->wheel->addOutcome($downNeighbor, $lineOutcome);
+      $this->wheel->addOutcome($downRightNeighbor, $lineOutcome);
+      $this->wheel->addOutcome($downRightRightNeighbor, $lineOutcome);
+    }
   }
-  
+
   /**
    * Create all dozen bets possible for the wheel
    **/
-  private function generateDozenBets() 
+  private function generateDozenBets()
   {
-    
+    for ($dozen = 0; $dozen < 3; $dozen++) {
+      $dozenOutcome = new Outcome("Dozen {$dozen + 1}", 2);
+      for ($numberCounter = 0; $numberCounter < 12; $numberCounter++) {
+        $number = (12 * $dozen) + $numberCounter + 1;
+        $this->wheel->addOutcome($number, $dozenOutcome);
+      }
+    }
   }
-  
+
   /**
    * Create all column possible for the wheel
    **/
-  private function generateColumnBets() 
+  private function generateColumnBets()
   {
-    
+    for ($column = 0; $column < 3; $column++) {
+      $columnOutcome = new Outcome("Column {$column + 1}", 2);
+      for ($row = 0; $row < 12; $row++) {
+        $number = (3 * $row) + $column + 1;
+        $this->wheel->addOutcome($number, $columnOutcome);
+      }
+    }
   }
-  
+
   /**
    * Create all split even money possible for the wheel
    **/
-  private function generateEvenMoneyBets() 
+  private function generateEvenMoneyBets()
   {
-    
-  }
-  
-  /**
-   * Create bets for the Zero Bin
-   **/
-  private function generateZeroBets() 
-  {
-    
-  }
-  
-  /**
-   * Create bets for the Double Zero Bin
-   **/
-  private function generateDoubleZeroBets() 
-  {
-    
+    $redOutcome = new Outcome("Red", 1);
+    $blackOutcome = new Outcome("Black", 1);
+    $evenOutcome = new Outcome("Even", 1);
+    $oddOutcome = new Outcome("Odd", 1);
+    $highOutcome = new Outcome("High", 1);
+    $lowOutcome = new Outcome("Low", 1);
+
+    for ($number=1; $number < 37; $number++) {
+      if ($this->isLow($number)) {
+        $this->wheel->addOutcome($number, $lowOutcome);
+      } else {
+        $this->wheel->addOutcome($number, $highOutcome);
+      }
+      if ($this->isEven($number)) {
+        $this->wheel->addOutcome($number, $evenOutcome);
+      } else {
+        $this->wheel->addOutcome($number, $oddOutcome);
+      }
+      if ($this->isRed($number)) {
+        $this->wheel->addOutcome($number, $redOutcome);
+      } else {
+        $this->wheel->addOutcome($number, $blackOutcome);
+      }
+    }
   }
 
-}  
+  /**
+   * Checks if a given number is under 19
+   *
+   * @return bool
+   */
+  private function isLow($number)
+  {
+    return $number < 19;
+  }
+
+  /**
+   * Checks if a given number is even
+   *
+   * @return bool
+   */
+  private function isEven($number)
+  {
+    return $number%2 == 0;
+  }
+
+  /**
+   * Checks if a given number is red
+   *
+   * @return bool
+   */
+  private function isRed($number)
+  {
+    $reds = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
+    return in_array($number, $reds);
+  }
+
+  /**
+   * Create bets for the Five Bet
+   **/
+  private function generateFiveBet()
+  {
+    $fiveOutcome = new Outcome("Five bet", 6);
+    $this->wheel->addOutcome(0, $fiveOutcome);
+    $this->wheel->addOutcome(1, $fiveOutcome);
+    $this->wheel->addOutcome(2, $fiveOutcome);
+    $this->wheel->addOutcome(3, $fiveOutcome);
+    $this->wheel->addOutcome(37, $fiveOutcome);
+  }
+
+
+}
--- roulette/tests/BinBuilderTest.php
+++ roulette/tests/BinBuilderTest.php
@@ -7,31 +7,37 @@ use Roulette\Outcome;
 use Roulette\BinBuilder;
 
 /**
- * The unit test should create several instances of Outcome, two instances of 
+ * The unit test should create several instances of Outcome, two instances of
  * Bin and establish that Bins can be constructed from the Outcomes.
  */
 final class BinBuilderTest extends TestCase
 {
-  
+
   protected $wheel;
-  
+
   protected $outcomes;
-  
+
   protected $bin;
-  
+
   protected function setUp()
   {
     $wheel = new Wheel();
     $binBuilder = new BinBuilder();
-    
+
     $this->wheel = $binBuilder->buildBins($wheel);
-    
+
     $this->outcomes[0] = [
       'straight' => new Outcome('0', 35),
       'split' => [],
-      'street' => '',
+      'street' => null,
+      'corner' => null,
+      'line' => null,
+      'dozen' => null,
+      'column' => null,
+      'evenMoney' => null,
+      'fiveBet' => new Outcome("Five Bet", 6)
     ];
-    
+
     $this->outcomes[12] = [
       'straight' => new Outcome('12', 35),
       'split' => [
@@ -40,8 +46,24 @@ final class BinBuilderTest extends TestCase
         new Outcome('Split 12-15', 17)
       ],
       'street' => new Outcome('Street 10-11-12', 11),
+      'corner' => [
+        new Outcome('Corner 8-9-11-12', 8),
+        new Outcome('Corner 11-12-14-15', 8),
+      ],
+      'line' => [
+        new Outcome('Line 7-8-9-10-11-12', 5),
+        new Outcome('Line 10-11-12-13-14-15', 5),
+      ],
+      'dozen' => new Outcome('Dozen 1', 2),
+      'column' => new Outcome('Column 3', 2),
+      'evenMoney' => [
+        new Outcome('Low', 1),
+        new Outcome('Red', 1),
+        new Outcome('Even', 1),
+      ],
+      'fiveBet' => null
     ];
-    
+
     $this->outcomes[21] = [
       'straight' => new Outcome('21', 35),
       'split' => [
@@ -50,8 +72,24 @@ final class BinBuilderTest extends TestCase
         new Outcome('Split 21-24', 17)
       ],
       'street' => new Outcome('Street 19-20-21', 11),
+      'corner' => [
+        new Outcome('Corner 17-18-20-21', 8),
+        new Outcome('Corner 20-21-23-24', 8),
+      ],
+      'line' => [
+        new Outcome('Line 16-17-18-19-20-21', 5),
+        new Outcome('Line 19-20-21-22-23-24', 5),
+      ],
+      'dozen' => new Outcome('Dozen 2', 2),
+      'column' => new Outcome('Column 3', 2),
+      'evenMoney' => [
+        new Outcome('High', 1),
+        new Outcome('Red', 1),
+        new Outcome('Odd', 1),
+      ],
+      'fiveBet' => null
     ];
-    
+
     $this->outcomes[28] = [
       'straight' => new Outcome('28', 35),
       'split' => [
@@ -60,48 +98,115 @@ final class BinBuilderTest extends TestCase
         new Outcome('Split 28-31', 17)
       ],
       'street' => new Outcome('Street 28-29-30', 11),
+      'corner' => [
+        new Outcome('Corner 25-26-28-29', 8),
+        new Outcome('Corner 28-29-31-32', 8),
+      ],
+      'line' => [
+        new Outcome('Line 25-26-27-28-29-30', 5),
+        new Outcome('Line 28-29-30-31-32-33', 5),
+      ],
+      'dozen' => new Outcome('Dozen 3', 2),
+      'column' => new Outcome('Column 1', 2),
+      'evenMoney' => [
+        new Outcome('High', 1),
+        new Outcome('Black', 1),
+        new Outcome('Even', 1),
+      ],
+      'fiveBet' => null
     ];
-    
+
     /*
       Will output the sequence:
       28, 12, 21, 0, 5, 37, 20, 27, 33, 13
-    */     
+    */
     srand(111);
-    
+
     $this->bin[0] = $this->wheel->next(); // 28
     $this->bin[1] = $this->wheel->next(); // 12
     $this->bin[2] = $this->wheel->next(); // 21
     $this->bin[3] = $this->wheel->next(); // 0
   }
-  
+
   public function testCanBuildStraightBets()
-  {  
+  {
     $this->assertTrue($this->bin[0]->hasOutcome($this->outcomes[28]['straight']));
     $this->assertTrue($this->bin[1]->hasOutcome($this->outcomes[12]['straight']));
     $this->assertTrue($this->bin[2]->hasOutcome($this->outcomes[21]['straight']));
     $this->assertTrue($this->bin[3]->hasOutcome($this->outcomes[0]['straight']));
   }
-  
+
   public function testCanBuildSplitBets()
   {
     $this->assertTrue($this->bin[0]->hasOutcome($this->outcomes[28]['split'][0]));
     $this->assertTrue($this->bin[0]->hasOutcome($this->outcomes[28]['split'][1]));
     $this->assertTrue($this->bin[0]->hasOutcome($this->outcomes[28]['split'][2]));
-    
+
     $this->assertTrue($this->bin[1]->hasOutcome($this->outcomes[12]['split'][0]));
     $this->assertTrue($this->bin[1]->hasOutcome($this->outcomes[12]['split'][1]));
     $this->assertTrue($this->bin[1]->hasOutcome($this->outcomes[12]['split'][2]));
-    
+
     $this->assertTrue($this->bin[2]->hasOutcome($this->outcomes[21]['split'][0]));
     $this->assertTrue($this->bin[2]->hasOutcome($this->outcomes[21]['split'][1]));
-    $this->assertTrue($this->bin[2]->hasOutcome($this->outcomes[21]['split'][2]));        
+    $this->assertTrue($this->bin[2]->hasOutcome($this->outcomes[21]['split'][2]));
   }
-  
+
   public function testCanBuildStreetBets()
   {
     $this->assertTrue($this->bin[0]->hasOutcome($this->outcomes[28]['street']));
     $this->assertTrue($this->bin[1]->hasOutcome($this->outcomes[12]['street']));
     $this->assertTrue($this->bin[2]->hasOutcome($this->outcomes[21]['street']));
   }
-  
+
+  public function testCanBuildCornerBets()
+  {
+    $this->assertTrue($this->bin[0]->hasOutcome($this->outcomes[28]['corner'][0]));
+    $this->assertTrue($this->bin[0]->hasOutcome($this->outcomes[28]['corner'][1]));
+    $this->assertTrue($this->bin[1]->hasOutcome($this->outcomes[12]['corner'][0]));
+    $this->assertTrue($this->bin[1]->hasOutcome($this->outcomes[12]['corner'][1]));
+    $this->assertTrue($this->bin[2]->hasOutcome($this->outcomes[21]['corner'][0]));
+    $this->assertTrue($this->bin[2]->hasOutcome($this->outcomes[21]['corner'][1]));
+  }
+
+  public function testCanBuildLineBets()
+  {
+    $this->assertTrue($this->bin[0]->hasOutcome($this->outcomes[28]['line'][0]));
+    $this->assertTrue($this->bin[0]->hasOutcome($this->outcomes[28]['line'][1]));
+    $this->assertTrue($this->bin[1]->hasOutcome($this->outcomes[12]['line'][0]));
+    $this->assertTrue($this->bin[1]->hasOutcome($this->outcomes[12]['line'][1]));
+    $this->assertTrue($this->bin[2]->hasOutcome($this->outcomes[21]['line'][0]));
+    $this->assertTrue($this->bin[2]->hasOutcome($this->outcomes[21]['line'][1]));
+  }
+
+  public function testCanBuildDozenBets()
+  {
+    $this->assertTrue($this->bin[0]->hasOutcome($this->outcomes[28]['dozen']));
+    $this->assertTrue($this->bin[1]->hasOutcome($this->outcomes[12]['dozen']));
+    $this->assertTrue($this->bin[2]->hasOutcome($this->outcomes[21]['dozen']));
+  }
+
+  public function testCanBuildColumnBet()
+  {
+    $this->assertTrue($this->bin[0]->hasOutcome($this->outcomes[28]['column']));
+    $this->assertTrue($this->bin[1]->hasOutcome($this->outcomes[12]['column']));
+    $this->assertTrue($this->bin[2]->hasOutcome($this->outcomes[21]['column']));
+  }
+
+  public function testCanBuildEvenMoneyBets()
+  {
+    $this->assertTrue($this->bin[0]->hasOutcome($this->outcomes[28]['evenMoney'][0]));
+    $this->assertTrue($this->bin[0]->hasOutcome($this->outcomes[28]['evenMoney'][1]));
+    $this->assertTrue($this->bin[0]->hasOutcome($this->outcomes[28]['evenMoney'][2]));
+    $this->assertTrue($this->bin[1]->hasOutcome($this->outcomes[12]['evenMoney'][0]));
+    $this->assertTrue($this->bin[1]->hasOutcome($this->outcomes[12]['evenMoney'][1]));
+    $this->assertTrue($this->bin[1]->hasOutcome($this->outcomes[12]['evenMoney'][2]));
+    $this->assertTrue($this->bin[2]->hasOutcome($this->outcomes[21]['evenMoney'][0]));
+    $this->assertTrue($this->bin[2]->hasOutcome($this->outcomes[21]['evenMoney'][1]));
+    $this->assertTrue($this->bin[2]->hasOutcome($this->outcomes[21]['evenMoney'][2]));
+  }
+
+  public function testCanBuildFiveBet()
+  {
+    $this->assertTrue($this->bin[3]->hasOutcome($this->outcomes[0]['fiveBet']));
+  }
 }

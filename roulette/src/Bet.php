609be546910fd355d3039ffa6af7fd6b810e7c09
Bruno Monteiro <bruno.monteiro@justdigital.com.br>
2018-02-21 22:10:30 -0300

    Bet class and tests

---
+++ roulette/src/Bet.php
@@ -0,0 +1,73 @@
+<?php
+declare(strict_types=1);
+
+namespace Roulette;
+
+use Roulette\Outcome;
+
+/**
+ * Associates an amount, an Outcome and a Player
+ */
+class Bet
+{
+  
+  /**
+   * @var int
+   */
+   protected $amountBet;
+   
+   /**
+    * @var Outcome
+    */
+   protected $outcome;
+   
+   /**
+    * Create a new Bet of a specific amount on a specific outcome.
+    */
+   public function __construct(int $amount, Outcome $outcome)
+   {
+     $this->amountBet = $amount;
+     $this->outcome = $outcome;
+   }
+  
+   /**
+    * Compute the amount won given the amount betted
+    *
+    * @return int
+    */
+   public function winAmount()
+   {
+     return $this->amountBet + $this->outcome->winAmount($this->amountBet);
+   }
+   
+   /**
+    * Compute the amount lost, e.g. the cost of placing the bet
+    *
+    * @return int
+    */
+   public function loseAmount()
+   {
+     return $this->amountBet;
+   }
+   
+   /**
+    * Easy-to-read representation of the Bet
+    *
+    * @return string
+    */
+   public function __toString()
+   {
+     return "{$this->amountBet} on {$this->outcome->getName()}";
+   }
+   
+   /**
+    * Easy-to-read representation of the Bet using another format
+    *
+    * @return string
+    */
+   public function representation()
+   {
+     return "Bet({$this->amount}, {$this->outcome->getName()})";
+   }
+   
+}
---
+++ roulette/tests/BetTest.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+use Roulette\Bet;
+use Roulette\Outcome;
+
+/**
+ * The unit test should create a couple instances of Outcome,
+ * and establish that the winAmount() and loseAmount() methods work correctly.
+ */
+final class BetTest extends TestCase
+{
+  
+  protected $outcomes;
+  
+  protected function setUp()
+  {
+    $this->outcomes['five'] = new Outcome('Five bet', 6);
+    $this->outcomes['zero'] = new Outcome('0', 35);
+    $this->outcomes['zerozero'] = new Outcome('00', 35);
+  }
+  
+  public function testCanGetWinAmount()
+  {  
+    $bet1 = new Bet(10, $this->outcomes['five']);
+    $bet2 = new Bet(7, $this->outcomes['zero']);
+    $bet3 = new Bet(19, $this->outcomes['zerozero']);
+    
+    $this->assertTrue($bet1->winAmount() == 70);
+    $this->assertTrue($bet2->winAmount() == 252);
+    $this->assertTrue($bet3->winAmount() == 684);
+  }
+  
+  public function testCanGetLoseAmount()
+  {
+    $bet1 = new Bet(3, $this->outcomes['five']);
+    $bet2 = new Bet(70, $this->outcomes['zero']);
+    $bet3 = new Bet(41, $this->outcomes['zerozero']);
+    
+    $this->assertTrue($bet1->loseAmount() == 3);
+    $this->assertTrue($bet2->loseAmount() == 70);
+    $this->assertTrue($bet3->loseAmount() == 41);
+  }
+  
+}

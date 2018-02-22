b77a8812a9f277788f247ec9ba5d04dedc5654f2
Bruno Monteiro <bruno.monteiro@justdigital.com.br>
2018-02-22 11:10:27 -0300

    Table class and tests

---
+++ roulette/src/Table.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+
+namespace Roulette;
+
+/**
+ * Has the responsibility to Keep the Bets created by the Player
+ * A table also has a betting limit, and the sum of all of a player’s
+ * bets must be less than or equal to this limit
+ */
+class Table
+{
+  
+  /**
+   * @var int
+   */
+  protected $limit;
+  
+  /**
+   * @var int
+   */
+  protected $minimum;
+  
+  /**
+   * @var Bet[]
+   */
+  protected $bets;
+  
+  /**
+   * Table constructor
+   */  
+  public function __construct()
+  {
+    $this->bets = [];
+  }
+  
+  /**
+   * Add a Bet to the Table
+   *
+   * @param Bet
+   *
+   * @return int
+   */
+  public function placeBet(Bet $bet)
+  {
+    return array_push($this->bets, $bet);
+  }
+  
+  /**
+   * Check if all bets pass the table limit rules
+   *
+   * @throws InvalidBetException   
+   */
+  public function isValid()
+  {
+    foreach ($this->bets as $bet) {
+      if (!$this->validateBet($bet)) {
+        throw new InvalidBetException("Invalid Bet", 1);        
+      }
+    }
+  }
+  
+  /**
+   * Check if a single bet pass the validation rules
+   *
+   * @param Bet
+   *
+   * @return bool
+   */
+  private function validateBet(Bet $bet)
+  {
+    
+  }
+  
+}
---
+++ roulette/tests/TableTest.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+use Roulette\Bet;
+use Roulette\Table;
+use Roulette\Outcome;
+
+/**
+ * The unit test should create at least two instances of Bet,
+ * and establish that these Bet s are managed by the table correctly.
+ */
+final class TableTest extends TestCase
+{
+  
+  public function testCanPlaceBet()
+  {
+    $five = new Outcome('Five bet', 6);
+    $zero = new Outcome('0', 35);
+    
+    $bet1 = new Bet(16, $five);
+    $bet2 = new Bet(34, $zero);
+    
+    $table = new Table();
+    
+    $this->assertTrue($table->placeBet($bet1) > 0);
+    $this->assertTrue($table->placeBet($bet2) > 0);
+  }
+  
+}
